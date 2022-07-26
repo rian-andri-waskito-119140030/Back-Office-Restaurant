b08c9f66d6292ca85edef666be8368584bfd02ce
EllyaRosanti <83719290+EllyaRosanti@users.noreply.github.com>
2022-07-26 16:57:15 +0700

    update

--- Front-Office-Restaurant/Front-end/resources/views/admin/transaksi/daftar_transaksi.blade.php
+++ Front-Office-Restaurant/Front-end/resources/views/admin/transaksi/daftar_transaksi.blade.php
@@ -1,50 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Transaksi</title>
-    <script
-      src="https://kit.fontawesome.com/387f5a3e4e.js"
-      crossorigin="anonymous"
-    ></script>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" type="text/css" href="admin.css" />
-    <style>
-      .box {
-        height: 50px;
-        background-color: #d9d9d9;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 20px;
-      }
-    </style>
-  </head>
-  <body>
-   @include('layouts.navbar_admin')
-    <div class="bg">
-      <div class="container">
-        <p class="text-uppercase" style="margin-top: 50px">KLIK NOMOR MEJA</p>
-        <div class="row">
-          @foreach ($data as $no_meja)
-          <div class="col-1">
-            <a href="/admin/transaksi/{{ $no_meja }}"><div class="box">{{ $no_meja }}</div> </a>
-          </div>
-          @endforeach
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Transaksi</title>
+  <script src="https://kit.fontawesome.com/387f5a3e4e.js" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="{{ URL::asset('admin.css')}}" />
+  <style>
+    .box {
+      height: 50px;
+      background-color: #d9d9d9;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+
+<body>
+  @include('layouts.navbar_admin')
+  <div class="bg">
+    <div class="container">
+      <p class="text-uppercase" style="margin-top: 50px">KLIK NOMOR MEJA</p>
+      <div class="row">
+        @foreach ($data as $no_meja)
+        <div class="col-1">
+          <a href="/admin/transaksi/{{ $no_meja }}">
+            <div class="box">{{ $no_meja }}</div>
+          </a>
         </div>
+        @endforeach
       </div>
     </div>
-  </body>
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
-  ></script>
+  </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
--- Front-Office-Restaurant/Front-end/resources/views/admin/transaksi/transaksi_meja.blade.php
+++ Front-Office-Restaurant/Front-end/resources/views/admin/transaksi/transaksi_meja.blade.php
@@ -1,77 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Transaksi1</title>
-    <script
-      src="https://kit.fontawesome.com/387f5a3e4e.js"
-      crossorigin="anonymous"
-    ></script>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" type="text/css" href="admin.css" />
-    <style>
-      .box {
-        height: 50px;
-        background-color: #d9d9d9;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 20px;
-        margin-left: 60px;
-      }
-    </style>
-  </head>
-  <body>
-    @include('layouts.navbar_admin')
-    <?php #dd($data); ?>
-    <div class="bg">
-        <div class="row">
-          <div class="col-3">
-           <div class="box">{{ count($data) }} Transaksi ditemukan</div>
-          </div>
-        </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Transaksi1</title>
+  <script src="https://kit.fontawesome.com/387f5a3e4e.js" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="{{ URL::asset('admin.css')}}" />
+  <style>
+    .box {
+      height: 50px;
+      background-color: #d9d9d9;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      margin-left: 60px;
+    }
+  </style>
+</head>
+
+<body>
+  @include('layouts.navbar_admin')
+  <?php #dd($data); 
+  ?>
+  <div class="bg">
+    <div class="row">
+      <div class="col-3">
+        <div class="box">{{ count($data) }} Transaksi ditemukan</div>
+      </div>
     </div>
-    @if (count($data)>1)
-    <ul class="pagination pagination-sm">
-      @foreach($data as $key => $item)
-      <li class="page-item"><button class="page-link" id="button_halanan" onclick="getPesanan({{ $key }})">{{ $key+1 }}</button></li>
-      @endforeach
-    </ul>
-    @endif
-    <div class="bg" id="konten"> 
-    </div>
-    <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
-  ></script>
+  </div>
+  @if (count($data)>1)
+  <ul class="pagination pagination-sm">
+    @foreach($data as $key => $item)
+    <li class="page-item"><button class="page-link" id="button_halanan" onclick="getPesanan({{ $key }})">{{ $key+1 }}</button></li>
+    @endforeach
+  </ul>
+  @endif
+  <div class="bg" id="konten">
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <script>
     const formatRupiah = (money) => {
-      return new Intl.NumberFormat('id-ID',
-        { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 }
-      ).format(money);
+      return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+      }).format(money);
     }
 
     function kembalian(total_harga) {
-      let bayar=document.getElementById('bayar').value;
-      document.getElementById('kembalian').innerHTML= formatRupiah(bayar-total_harga);
+      let bayar = document.getElementById('bayar').value;
+      document.getElementById('kembalian').innerHTML = formatRupiah(bayar - total_harga);
     }
 
     getPesanan(0);
 
     function getPesanan(item) {
-      let data= <?php echo json_encode($data); ?>;
+      let data = <?php echo json_encode($data); ?>;
       console.log(data[item]);
-      let pesanan=data[item];
+      let pesanan = data[item];
 
-      let text=`<div class="container">
+      let text = `<div class="container">
             <div class="row">
               <div class="col-3">
                 <div class="box">Id Pesanan: ${pesanan.id_pesanan}</div>
@@ -95,25 +88,25 @@
               <tr>
                 <th scope="row">`;
       pesanan.menu_dipesan.forEach(element => {
-        text+=`${element.nama_menu} </br>`
+        text += `${element.nama_menu} </br>`
       });
-      text+=`</th>
+      text += `</th>
                 <td>`;
       pesanan.menu_dipesan.forEach(element => {
-        text+=`${formatRupiah(element.harga_jual)} </br>`
+        text += `${formatRupiah(element.harga_jual)} </br>`
       });
-      text+=`</td>
+      text += `</td>
               <td>`
       pesanan.menu_dipesan.forEach(element => {
-        text+=`${element.jumlah} </br>`
+        text += `${element.jumlah} </br>`
       });
-      text+=`</td>
+      text += `</td>
                 <td>`;
       pesanan.menu_dipesan.forEach(element => {
-        text+=`${formatRupiah(element.harga_peritem)} </br>`
+        text += `${formatRupiah(element.harga_peritem)} </br>`
       });
-      
-      text+=`   </td>
+
+      text += `   </td>
               </tr>
             </tbody>
           </table>
@@ -144,7 +137,8 @@
 
       document.getElementById("konten").innerHTML = text;
 
-    } 
+    }
   </script>
-  </body>
+</body>
+
 </html>
